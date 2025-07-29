8b4b5d49c491593e64063a4178a6cda8f7a555d3
themonkeyman355 <ewanmackerracher@gmail.com>
2025-07-29 22:29:32 +0100

    Fixed router error

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+// AFTER
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Website-Project">
-    <App />
-  </BrowserRouter>
+  <App />
 );
