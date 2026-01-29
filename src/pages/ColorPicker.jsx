ed8bd5840247dffb8553c820a7e2e637fa6c41fe
ewanhmack05 <ewan.mackerracher@cirdan.com>
2026-01-29 12:27:31 +0000

    added page and routing

---
+++ src/pages/ColorPicker.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ColorPicker() {
+    return (
+        <div>
+            {/* <ColorPicker /> */}
+            ColorPicker WIP...
+        </div>
+    );
+}
