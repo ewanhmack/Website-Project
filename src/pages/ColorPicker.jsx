93eda00a243307c3f393dcf9dcdca56ac7e8ea14
Ewan MacKerracher <107613842+ewanhmack@users.noreply.github.com>
2026-01-30 22:14:58 +0000

    Added ColorPicker package through tarball file aswell as routing errors (#2)

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { ColorPicker, useColor } from 'react-colour-palette';
+import 'react-colour-palette/dist/index.css';
 
-export default function ColorPicker() {
-    return (
-        <div>
-            {/* <ColorPicker /> */}
-            ColorPicker WIP...
-        </div>
-    );
+export default function ColorPickerPage() {
+  const [color, setColor] = useColor('hex', '#ff0000');
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', paddingTop: 40 }}>
+      <div>
+        <ColorPicker
+          color={color}
+          onChange={setColor}
+          onChangeComplete={setColor}
+          dark
+        />
+        <pre style={{ marginTop: 20 }}>
+          {JSON.stringify(color, null, 2)}
+        </pre>
+      </div>
+    </div>
+  );
 }
